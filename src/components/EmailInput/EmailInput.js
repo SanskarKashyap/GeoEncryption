dd973aa8e8c683aab28bba38deac3557ddffc355
SanskarKashyap <2005825@kiit.ac.in>
2023-12-12 12:56:49 +0530

    isEmailValid

--- src/components/EmailInput/EmailInput.js
+++ src/components/EmailInput/EmailInput.js
@@ -1,14 +1,39 @@
+
 import React, { useState } from 'react';
 
 const EmailInput = () => {
   const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const isEmailValid = () => {
+    // Basic email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    const newEmail = event.target.value;
+
+    // Check for illegal characters using a basic validation
+    if (!isEmailValid()) {
+      setError('Invalid email address');
+    } else {
+      setError('');
+    }
+
+    setEmail(newEmail);
   };
 
   return (
-    <input type="email" value={email} onChange={handleEmailChange} placeholder="Recipient's Email" />
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+        placeholder="Recipient's Email"
+      />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
   );
 };
 
