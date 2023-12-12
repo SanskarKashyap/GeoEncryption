38d13665b29cc200d5f5739d4d2b929913fb55dc
SanskarKashyap <2005825@kiit.ac.in>
2023-12-12 12:59:30 +0530

    updateEncryptedKey

--- src/components/KeyInput/KeyInput.js
+++ src/components/KeyInput/KeyInput.js
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const KeyInput = () => {
-  const [key, setKey] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [encryptedKey, setEncryptedKey] = useState('');
 
-  const handleKeyChange = (event) => {
-    setKey(event.target.value);
+  useEffect(() => {
+    // Function to apply Base64 encryption
+    const encryptKey = (word) => {
+      return btoa(word); // Using btoa for Base64 encoding
+    };
+
+    // Update the encrypted key whenever the user input changes
+    const updateEncryptedKey = () => {
+      const encrypted = encryptKey(userInput);
+      setEncryptedKey(encrypted);
+    };
+
+    updateEncryptedKey();
+  }, [userInput]);
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
   };
 
   return (
-    <input type="text" value={key} onChange={handleKeyChange} placeholder="Encryption Key" />
+    <div>
+      <input
+        type="text"
+        value={userInput}
+        onChange={handleInputChange}
+        placeholder="Enter a Random Word"
+      />
+      <p>Encrypted Key: {encryptedKey}</p>
+    </div>
   );
 };
 
